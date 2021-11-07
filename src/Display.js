37196533df64e84f5bd4c5e74e326b2fe31b2488
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2021-11-07 10:17:38 +0300

    Display an array

--- src/Display.js
+++ src/Display.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Dated from './Date';
+import ArrayInput from './ArrayInput';
 
 function Display({ input }) {
   return (
     <div id="display">
       { (!!input === false || input === '0')
        && <Dated /> }
+      { (input.includes(' '))
+       && <ArrayInput input={input.split(' ')} /> }
+      { (!input.includes(' '))
+       && <div>{input}</div>}
     </div>
   );
 }
 
 Display.defaultProps = {
-  input: 'Home',
+  input: ' ',
 };
 
 Display.propTypes = {
