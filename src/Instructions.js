a72a1d6be26d836a40380d8325957e64488b2870
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2021-11-07 10:47:58 +0300

    Add styles

--- src/Instructions.js
+++ src/Instructions.js
@@ -5,15 +5,15 @@ function Instructions() {
     <div>
       <p>
         When a user enters a
-        <span>false</span>
+        <span> false </span>
         value AKA
         <span> no value is entered </span>
-        the date will be displayed. This date updates every second
+        the date will be displayed. This date updates every second.
       </p>
       <p>
         When an input with spaces is entered,
         it&apos;s converted into an array and the individual
-        elements of the array is displayed
+        elements of the array is displayed.
       </p>
       <p>If the input is not of the two, it will be displayed as is.</p>
     </div>
